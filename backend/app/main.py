16c998e1b2d9a9378cb6d230584cc21b4a3fd9c2
octoverse24 <hugo.orlando2050@gmail.com>
2024-08-04 14:55:34 -0300

    fastapi init

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,15 @@
-print("FastAPI")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{name}")
+def read_item(name: str):
+    return {"name": f"Hello {name}"}
